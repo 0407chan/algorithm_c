5cb16210b9ff0ff08067a07c371cf8dd445ab998
이찬호 <chanho@woowahan.com>
2022-01-11 22:27:39 +0900

    2741

---
+++ backjun/fin_b3_2741.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
