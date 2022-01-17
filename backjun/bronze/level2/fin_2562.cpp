6aaf3ed8c5f3a883ce23f4c7dc47a70edc112f8c
이찬호 <0407chan@naver.com>
2022-01-17 22:24:44 +0900

    2562 done

--- backjun/bronze/level2/fin_2562.cpp
+++ backjun/bronze/level2/fin_2562.cpp
@@ -6,21 +6,23 @@ int main()
     cin.tie(NULL);
     cin.sync_with_stdio(false);
 
-    int num, index;
-    int max = -1;
+    int a, b, c, res, nums[10];
+    fill_n(nums, 10, 0);
 
-    for (int i = 0; i < 9; i++)
+    cin >> a >> b >> c;
+    res = a * b * c;
+    while (true)
     {
-        cin >> num;
-        if (max < num)
-        {
-            max = num;
-            index = i;
-        }
+        nums[res % 10]++;
+        res /= 10;
+        if (res == 0)
+            break;
     }
 
-    cout << max << "\n"
-         << index + 1 << "\n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << nums[i] << "\n";
+    }
 
     return 0;
 }
