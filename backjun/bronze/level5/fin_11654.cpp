cda4e39fb4c03eb39adf53b797448a1622df0606
이찬호 <0407chan@naver.com>
2022-01-19 23:46:26 +0900

    11654

---
+++ backjun/bronze/level5/fin_11654.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+    char a;
+    cin >> a;
+    cout << (int)a;
+
+    return 0;
+}
